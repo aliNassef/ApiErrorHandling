b2c75ffc14f3f5b9a27a6bd4d116ced97e4b18dc
Ali Nassef <102700419+aliNassef@users.noreply.github.com>
2025-12-03 05:21:54 +0200

    Add factory method to create ErrorModel from JSON

--- error_model.dart
+++ error_model.dart
@@ -1,4 +1,9 @@
 class ErrorModel {
   final String message;
   const ErrorModel({required this.message});
+  factory ErrorModel.fromJson(Map jsonData) {
+    return ErrorModel(
+      errorMessage: jsonData["message"] ?? 'Unknown error',
+    );
+  }
 }
